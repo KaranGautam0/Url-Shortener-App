6723dba33d1a6f04bae695a9340a014688942340
Karan Gautam <karangautam.gpm@gmail.com>
2024-05-14 15:58:21 +0530

    Server Setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Welcome to Short url generator api");
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({ Message: "Welcome to short url generator api" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started at port: ${PORT}`);
+});
