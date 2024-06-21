ed09d22769172deb285ae9c8771eb3d19deba726
Karan Gautam <karangautam.gpm@gmail.com>
2024-06-21 18:59:23 +0530

    handleGenerateNewShortURL error handling using try-catch

--- src/controllers/url.js
+++ src/controllers/url.js
@@ -2,17 +2,21 @@ const shortid = require("shortid");
 const URL = require("../models/url");
 
 async function handleGenerateNewShortURL(req, res) {
-  const { url } = req.body;
-  if (!url) return res.status(400).json({ error: "url is required" });
-  const shortID = shortid();
+  try {
+    const { url } = req.body;
+    if (!url) return res.status(400).json({ error: "url is required" });
+    const shortID = shortid();
 
-  await URL.create({
-    shortID: shortID,
-    redirectURL: url,
-    visitHistory: [],
-  });
+    await URL.create({
+      shortID: shortID,
+      redirectURL: url,
+      visitHistory: [],
+    });
 
-  res.status(200).json({ message: "URL Saved.", shortID: shortID });
+    res.status(201).json({ message: "URL Saved.", shortID: shortID });
+  } catch (error) {
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 }
 
 async function Analytics(req, res) {
@@ -20,7 +24,7 @@ async function Analytics(req, res) {
   const result = await URL.findOne({ shortID });
 
   return res.json({
-    shortID : result.shortID,
+    shortID: result.shortID,
     LongURL: result.redirectURL,
     totalChicks: result.visitHistory.length,
     analytics: result.visitHistory,
