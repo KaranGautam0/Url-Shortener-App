ef0cded9e27dba54e234f1fcb3a89503c49fa320
Karan Gautam <karangautam.gpm@gmail.com>
2024-06-23 15:36:04 +0530

    Nav bar button functionnality

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -1,1 +1,18 @@
-console.log("let start app functionnality!!");
+//===> nav bar button functionnality
+
+const homeBtn = document.querySelector("#Home-btn");
+const historyBtn = document.querySelector("#History-btn");
+const showHistory = document.querySelector(".history");
+const showHome = document.querySelector(".Home");
+
+historyBtn.addEventListener("click", () => {
+  showHistory.style.display = "block";
+  showHome.style.display = "none";
+});
+
+homeBtn.addEventListener("click", () => {
+  showHome.style.display = "block";
+  showHistory.style.display = "none";
+});
+
+// done nav bar button functionnality <===\\
